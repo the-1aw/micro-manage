9758e71a76094f976a1a3e47936f5f393d64e173
the-1aw <bguilliod@gmail.com>
2020-02-14 16:23:38 -0800

    add: husky + lint-staged

--- bin/micro-manager.js
+++ bin/micro-manager.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 const program = require('commander');
 
-const microManager = require("../lib");
 const currentVersion = require('../package.json').version;
 
 program.version(currentVersion);
 
 program.parse(process.argv);
-
