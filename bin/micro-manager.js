724fa3705812a9e7983427a74c82c464950456a0
the-1aw <bguilliod@gmail.com>
2020-02-14 16:41:10 -0800

    add: first command

--- bin/micro-manager.js
+++ bin/micro-manager.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 const program = require('commander');
 
+const microManage = require('../lib');
 const currentVersion = require('../package.json').version;
 
 program.version(currentVersion);
+
+program.command('init')
+  .description('initialize micro-manage and create .mmrc')
+  .action(microManage.init);
 
 program.parse(process.argv);
